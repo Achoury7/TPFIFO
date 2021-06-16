0f87af9c9d93860acccc8de98752247026b75852
Achoury7 <56994048+Achoury7@users.noreply.github.com>
2021-06-16 17:48:18 +0200

    Add files via upload
    TP FIFO

---
+++ TPFIFO.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define LISTE char "bonjour"
+//File FIFO
+//Initialisation de la file
+
+
+typedef struct Element_debut;
+struct Element_char* p_suivant;
+	struct Liste_char;
+{
+	int LISTE
+		c_taille = 0;
+};
+
+int main{
+
+   if (LISTE) {
+		Liste->element = NULL;
+		Liste->c_taille = 0;
+	}
+}
+
+
+//Insertion d'un element dans la file	
+if (element && LISTE) {
+	element->p_suivant = NULL;
+	LISTE->element = element_debut;
+	LISTE->c_"b";
+}
+
+//Enlevement d'un element dans la file	
+int destructListe(LISTE) {
+
+	if (LISTE->c_taille == 0)
+		return 0;
+
+	while (LISTE->c_taille > 0)
+		suppDebut(LISTE);
+
+	return 1;
+}
+
+//Affichage de la file
+if (InsertListeVide(LISTE) {
+	printf("Voici la liste de la file:",  LISTE \n);
+}
+
+
+//Retournage de la taille de la file
+
+void afficheListe(Liste_char* LISTE) {
+
+	Element_char*  = NULL;
+		LISTE = LISTE->element_debut;
+
+		while (LISTE!= Null) {
+			print("%d\n");
+		}
+}
+
